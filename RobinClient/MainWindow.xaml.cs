9d25d2e1edfa6d0eac031d49ce1d77013335bfb2
The Alchemist <at.thealchemist@gmail.com>
2019-10-28 19:47:22 +0300

    added client

--- RobinClient/MainWindow.xaml.cs
+++ RobinClient/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RobinClient.utils;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RobinClient
 {
@@ -20,9 +9,47 @@ namespace RobinClient
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Sender sender;
+
+        public ObservableCollection<string> ResponseList { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            ResponseList = new ObservableCollection<string>();
+
+            DataContext = this;
+
+            sender = new Sender("ws://localhost:8765");
+            sender.OnResult += ShowResult;
+        }
+
+        private void ShowResult(string value)
+        {
+            listViewResponses.Dispatcher.Invoke(() =>
+            {
+                if (!ResponseList.Contains(value))
+                {
+                    ResponseList.Add(value);
+                }
+            });
+
+        }
+
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            string value = textBoxValue.Text;
+
+            int requestsCount = listViewRequests.Items.Count;
+            int responsesCount = listViewResponses.Items.Count;
+
+            listViewRequests.Items.Add($"Input [{requestsCount}]: {value}");
+
+            this.sender.Send(value);
+
+            textBoxValue.Clear();
+
         }
     }
 }
---
+++ RobinClient/utils/Sender.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace RobinClient.utils
+{
+    public class Sender
+    {
+        private WebSocket client;
+
+        public Action<string> OnResult;
+
+        private string url;
+
+        public Sender(string url)
+        {
+            this.url = url;
+            client = new WebSocket(this.url);
+            client.Log.Level = LogLevel.Fatal;
+        }
+
+        public void Send(string value)
+        {
+            client.OnOpen += (_, e) => Console.WriteLine("ws opened");
+            client.OnMessage += (_, e) => OnResult(e.Data);
+            client.OnClose += (_, e) =>
+            {
+                Task.Run(() =>
+                {
+                    while (!client.IsAlive)
+                    {
+                        Task.Delay(1000).Wait();
+                        client.Connect();
+                    }
+                });
+            };
+
+            client.Connect();
+            if (client.IsAlive)
+            {
+                client.Send(value);
+            }
+        }
+    }
+}
